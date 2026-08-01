04f8ef8469141f81b22ecbd7983fb6e18b966710
Yash Tailor <yashtailor78@gmail.com>
2026-08-01 12:06:23 +0530

    create auth screen

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,10 +1,7 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { Stack } from "expo-router";
 
-export default function _layout() {
-  return (
-    <View>
-      <Text>Auth_layout</Text>
-    </View>
-  )
+export default function Authlayout() {
+  return ( <Stack screenOptions={{headerShown: false}} />)
+  
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,8 @@
 import { Redirect, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import {GestureHandlerRootView} from 'react-native-gesture-handler'
-import * as SplashScreen from 'expo-splash-screen';
 
-SplashScreen.preventAutoHideAsync();
+
 
 function AuthGuard(){
   const {isSignedIn} = {isSignedIn: false}
